3c1cc92286e455d8828559af0e81d1b6b93e28ed
eitanparan <eitanazran@gmail.com>
2025-09-03 23:12:13 +0300

    this is an automatic commit message 2025-09-03-02-00-00

--- main.tf
+++ main.tf
@@ -45,3 +45,5 @@ this is an automatic newline 2025-09-03-00-00-00
 this is an automatic newline 2025-09-03-00-00-00
 
 this is an automatic newline 2025-09-03-01-00-00
+
+this is an automatic newline 2025-09-03-02-00-00
