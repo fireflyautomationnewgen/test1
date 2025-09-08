56caff8152261347ed6d0d5a9addc2659ce766c6
eitanparan <eitanazran@gmail.com>
2025-09-08 11:10:01 +0300

    this is an automatic commit message 2025-09-08-04-00-00

--- main.tf
+++ main.tf
@@ -91,3 +91,5 @@ resource "null_resource" "test" {
 # this is an automatic newline 2025-09-08-03-00-00
 
 # this is an automatic newline 2025-09-08-04-00-00
+
+# this is an automatic newline 2025-09-08-04-00-00
